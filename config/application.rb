bf778cf127b001a235753903cb426e7e955687ef
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2018-05-12 15:35:46 +0700

    Update configuration

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,11 @@ module RspecTrainingGround
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
-      g.test_framework :rspec, fixtures: false, view_specs: false, helper_specs: false, routing_specs: false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
     end
   end
 end
