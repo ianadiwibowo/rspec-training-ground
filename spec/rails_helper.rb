ac461d935b2abf108a7b8f67e2c69e9c8e919116
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2018-05-26 21:39:34 +0700

    Configure Paperclip test upload folder, part 2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'paperclip/matchers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 SimpleCov.start
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -19,7 +19,14 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  # Add support for Paperclip's Shoulda Matchers
   config.include Paperclip::Shoulda::Matchers
+
+  # Clean up file uploads when test suite is finished
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_uploads/"])
+  end
 end
 
 Shoulda::Matchers.configure do |config|
